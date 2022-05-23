4b28d3557f0bb88d6dc4909a249f1a5ea3094ebd
mahdi parivash <mahdiparivash@protonmail.com>
2022-05-23 09:40:07 +0430

    fix auth input problem

--- components/AuthInputs.tsx
+++ components/AuthInputs.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { authModalState } from "../atoms/authModalAtom";
 import Login from "./Login";
+import SignUP from "./SignUp";
 
 type AuthInputsProps = {};
-// const modalState = useRecoilValue(authModalState);
 
 const AuthInputs: React.FC<AuthInputsProps> = () => {
+  const modalState = useRecoilValue(authModalState);
   return (
     <div className="flex flex-col align-center mt-4 w-full">
-      {/* {modalState.view === "login" && <Login />} */}
-      <Login />
+      {modalState.view === "login" && <Login />}
+      {modalState.view === "Sign Up" && <SignUP />}
     </div>
   );
 };
