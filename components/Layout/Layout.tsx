1eeea80a0dcd38c0a20b5c386d10e45af9aa0e7c
mahdi parivash <mahdiparivash@protonmail.com>
2022-07-24 09:15:17 +0430

    Refactor Project with chakra ui

---
+++ components/Layout/Layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  );
+};
+export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,18 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../chakra/theme";
+import Layout from "../components/Layout/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <ChakraProvider theme={theme}>
+      <RecoilRoot>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RecoilRoot>
+    </ChakraProvider>
   );
 }
 export default MyApp;
